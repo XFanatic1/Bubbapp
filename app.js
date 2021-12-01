b09bc7a332f7473c1112ec29d9c677564f95d57b
XFanatic1 <94031356+XFanatic1@users.noreply.github.com>
2021-11-30 20:09:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,47 @@ var MongoClient = require('mongodb').MongoClient;
 const { Router } = require("express");
 var url = "mongodb://localhost:27017/";
 
+const mongoose = require('mongoose'); //PLEASE CHANGE everything to mongoose standard
+const { URLSearchParams } = require("url");
+mongoose.connect('mongodb://localhost:27017/mydb', {useNewUrlParser: true});
+
+const Schema = mongoose.Schema
+const BookSchema = new Schema({
+  name : String,
+  color: String,
+  img: String
+})
+const Model = mongoose.model
+const Book = Model('datas',BookSchema)
+
+
 const app = express();
 app.use(bodyParser.urlencoded({
     extended:true
 }));
+
+app.set('view engine', 'ejs');
+
+app.use( express.static( "public" ) );
+
 //variables made global
 var username
 var password
 
+//socket.io code
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/c', (req, res) => {
+  res.sendFile(__dirname + '/test.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+//end
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
@@ -27,11 +60,11 @@ app.post("/", function(req, res) {
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       var dbo = db.db("mydb");
-      var myobj = { name: num1, Password: num2, img: '/Users/eriktuft/Desktop/database-pictures/muchcold-1tbom1k.png'};
+      var myobj = { name: num1, Password: num2, img: '/muchcold-1tbom1k.png'};
       dbo.collection("datas").insertOne(myobj, function(err, res) {
         if (err) throw err;
         console.log("1 document inserted");
-        
+
       });
     });
   }
@@ -70,11 +103,18 @@ app.post("/a", function(req, res) {
 app.listen(8080, function(){
   console.log("server is running on port 8080");
 })
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+
+
 
 app.get('/b', function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+  var user
+  Book.findOne({name: username},(err,result)=>{
+    user = result
+
+  })
+  setTimeout(() => {
+    res.render(__dirname + "/profile.ejs", {user})
+  }, 1000);
 })
 app.get('/homepage', function(req, res) {
 
@@ -83,23 +123,22 @@ app.get('/homepage', function(req, res) {
 
 app.post('/b', function(req, res) {
 
-  var http = require('http');
+  // var http = require('http');
   var formidable = require('formidable');
   var fs = require('fs');
 
   var form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
     var oldpath = files.filetoupload.filepath;
-    var newpath = '/Users/eriktuft/Desktop/database-pictures/' + files.filetoupload.originalFilename;
+    var newpath = '/Users/eriktuft/Desktop/untitled folder/public/' + files.filetoupload.originalFilename;
+    var imgname = files.filetoupload.originalFilename;
+    console.log(imgname)
+    Book.updateOne({name: username},{img: imgname},(err,res)=>{
+      console.log('Updated');
+    })
     fs.rename(oldpath, newpath, function (err) {
       if (err) throw err;
-      res.sendFile(__dirname + "/index.html")
+      res.redirect('/b')
     });
-    MongoClient.connect(url, function(err, db) {
-      var dbo = db.db("mydb");
-      dbo.collection('datas').find({name: username, Password: password}).forEach(function(doc) {
-        doc.//method
-      });                                                                                                                                                                                 
-  })
-})                 
+  })                 
 })
